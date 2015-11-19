c22fcdd8bd26ac5d8f6a4428ed945da6813aff5f
charlycolombain <charlycolombain@gmail.com>
2015-11-19 11:56:56 +0100

    menu changes 19/11

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -4,9 +4,12 @@ using UnityEngine.UI;
 
 public class Menu:MonoBehaviour{
 
-	public Button newGameButton;
+	public Button newStoryGameButton;
+	public Button customGameButton;
+	public Button continueStoryGameButton;
 
 	void Start(){
-
+		newStoryGameButton.onClick.AddListener (GameManager.NewStoryGame);
+		continueStoryGameButton.onClick.AddListener (GameManager.ContinueStoryGame);
 	}
 }
