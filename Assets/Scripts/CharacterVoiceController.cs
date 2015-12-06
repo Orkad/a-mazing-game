a16a3b50fb198b47ffdb83eb616b83dc0cb95375
RPoulalier <remy.poulalier@etu.univ-lyon1.fr>
2015-12-06 16:11:25 +0100

    Modify the CharacterVoiceController script. Update grammar.txt
    Signed-off-by: RPoulalier <remy.poulalier@etu.univ-lyon1.fr>

--- Assets/Scripts/CharacterVoiceController.cs
+++ Assets/Scripts/CharacterVoiceController.cs
@@ -1,33 +1,44 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Diagnostics;
 
 public class CharacterVoiceController : MonoBehaviour {
+
     public UDP_RecoServer speechReco;
+    private static string outputPath = "RecoServeurX64.exe";
+    Process foo = new Process();
+    private Rigidbody rb { get { return GetComponent<Rigidbody>(); } }
+    public KeyCode m_NorthKey = KeyCode.Z;
+    public KeyCode m_EastKey = KeyCode.D;
+    public KeyCode m_SouthKey = KeyCode.S;
+    public KeyCode m_WestKey = KeyCode.Q;
+    public float m_Force = 1000f;
+    public float m_MaxSpeed = 3f;
+
+    void Start()
+    {
+        foo.StartInfo.FileName = outputPath;
+        foo.Start();
+    }
 
 	void Update () {
-        if (speechReco.UDPGetPacket() == "Up")
+        switch (speechReco.UDPGetPacket())
         {
-            transform.Translate(Vector3.forward * Time.deltaTime);
-        }
-        if (speechReco.UDPGetPacket() == "Left")
-        {
-            transform.Translate(Vector3.left * Time.deltaTime);
-        }
-        if (speechReco.UDPGetPacket() == "Right")
-        {
-            transform.Translate(Vector3.right * Time.deltaTime);
-        }
-        if (speechReco.UDPGetPacket() == "Down")
-        {
-            transform.Translate(Vector3.back * Time.deltaTime);
-        }
-        if (Input.GetKey(KeyCode.Escape))
-        {
-            speechReco.set_strReceive("Close");
-        }
-        if (speechReco.UDPGetPacket() == "Stop")
-        {
-            transform.position = transform.position;
+            case "Up":
+                transform.Translate(Vector3.forward * Time.deltaTime/1.5f);
+                break;
+            case "Left":
+                transform.Translate(Vector3.left * Time.deltaTime/1.5f);
+                break;
+            case "Right":
+                transform.Translate(Vector3.right * Time.deltaTime/1.5f);
+                break;
+            case "Down":
+                transform.Translate(Vector3.back * Time.deltaTime/1.5f);
+                break;
+            case "Stop":
+                transform.position = transform.position;
+                break;
         }
     }
 }
